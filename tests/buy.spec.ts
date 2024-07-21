e6e3f8d1e6d6497259bfc65d25faa4abaa2fd15f
GCortesGustavo <gustavocortes934@gmail.com>
2024-07-21 16:19:19 -0300

    Modificacion de test

--- tests/buy.spec.ts
+++ tests/buy.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Añadir producto al carrito', async ({ page }) => {
     await page.goto('https://automationexercise.com/');
 
     await page.mouse.wheel(0, 500);
+    await page.hover('.choose > .nav > li > a')
     await page.locator('.choose > .nav > li > a').first().click();
+    // await page.locator('#quantity').click();
+
+    //hacer click 2 veces
     await page.locator('#quantity').click();
+    await page.locator('#quantity').fill('2');  
+
     await page.getByRole('button', { name: ' Add to cart' }).click();
     await page.getByRole('button', { name: 'Continue Shopping' }).click();
     await expect (page.locator("#modal-content")).toBeHidden()
